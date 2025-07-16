f1757a5b6c74314eca414fb4df18014fd2101867
Tom Bracey <tombracey22@gmail.com>
2025-07-16 23:57:43 +0100

    Pushed by submit.sh

---
+++ problems/sql/average_selling_price.sql
@@ -0,0 +1,40 @@
+-- Table: Prices
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | product_id    | int     |
+-- | start_date    | date    |
+-- | end_date      | date    |
+-- | price         | int     |
+-- +---------------+---------+
+-- (product_id, start_date, end_date) is the primary key (combination of columns with unique values) for this table.
+-- Each row of this table indicates the price of the product_id in the period from start_date to end_date.
+-- For each product_id there will be no two overlapping periods. That means there will be no two intersecting periods for the same product_id.
+ 
+
+-- Table: UnitsSold
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | product_id    | int     |
+-- | purchase_date | date    |
+-- | units         | int     |
+-- +---------------+---------+
+-- This table may contain duplicate rows.
+-- Each row of this table indicates the date, units, and product_id of each product sold. 
+ 
+
+-- Write a solution to find the average selling price for each product. average_price should be rounded to 2 decimal places. If a product does not have any sold units, its average selling price is assumed to be 0.
+
+-- Return the result table in any order.
+
+SELECT 
+  Prices.product_id, 
+  ROUND(COALESCE(SUM(price * units), 0) * 1.0 / COALESCE(SUM(units), 1), 2) AS average_price
+FROM Prices
+LEFT JOIN UnitsSold
+  ON Prices.product_id = UnitsSold.product_id
+  AND purchase_date BETWEEN start_date AND end_date
+GROUP BY Prices.product_id
