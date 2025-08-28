75f8370a7a682c6dfa3226d40551e84e9d191de7
Tom Bracey <tombracey22@gmail.com>
2025-08-28 23:26:05 +0100

    Pushed by submit.sh

---
+++ problems/sql/percentage_of_users_attended_a_contest.sql
@@ -0,0 +1,36 @@
+-- Table: Users
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | user_id     | int     |
+-- | user_name   | varchar |
+-- +-------------+---------+
+-- user_id is the primary key (column with unique values) for this table.
+-- Each row of this table contains the name and the id of a user.
+ 
+
+-- Table: Register
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | contest_id  | int     |
+-- | user_id     | int     |
+-- +-------------+---------+
+-- (contest_id, user_id) is the primary key (combination of columns with unique values) for this table.
+-- Each row of this table contains the id of a user and the contest they registered into.
+ 
+
+-- Write a solution to find the percentage of the users registered in each contest rounded to two decimals.
+
+-- Return the result table ordered by percentage in descending order. In case of a tie, order it by contest_id in ascending order.
+
+-- The result format is in the following example.
+
+SELECT 
+    Register.contest_id,
+    ROUND(COUNT(Register.user_id) * 100.0 / (SELECT COUNT(*) FROM Users), 2) AS percentage
+FROM Register
+GROUP BY Register.contest_id
+ORDER BY percentage DESC, Register.contest_id ASC;
